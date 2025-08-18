2614de4f8bad80037a6c31858a92cf9ee5c24d78
Jhoel0521 <jhoel0521@gmail.com>
2025-08-18 09:49:40 -0400

    mejora: simplificar el archivo index.php eliminando encabezados CORS y configuraciones innecesarias

--- index.php
+++ index.php
@@ -1,23 +1,13 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Headers: X-Mi-Token, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization, Cache-Control');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+require_once 'bootstrap.php';
 
-// Respuesta para la solicitud OPTIONS (preflight)
-$method = $_SERVER['REQUEST_METHOD'];
-if ($method == "OPTIONS") {
-    exit();
-}
-require_once __DIR__ . '/Core/configuration.php';
-require_once __DIR__ . '/Core/Router.php';
 $_ENV = loadEnv(__DIR__ . DIRECTORY_SEPARATOR . '.env');
 
 use Core\Router;
 use Core\Request;
 // Cargar rutas desde la carpeta /routes
 $request = new Request();
-Router::loadRoutes(BASE_ROUTE . 'routes');
+Router::loadRoutes(BASE_PATH . 'routes');
 
 // Obtener la ruta solicitada
 $requestedRoute = strtok($_SERVER['REQUEST_URI'], '?'); // Elimina query params
